2c10bb205172ec2c440d26c21fd06cd5d023a2bb
djvill <d.vill@pitt.edu>
2024-01-07 18:42:56 -0500

    Add horizontal scroll with vanishing buttons to tablist

--- assets/js/tablist-script.js
+++ assets/js/tablist-script.js
@@ -19,3 +19,42 @@ function tablistClickHandle(evt, tablistName, tabTitle) {
   document.getElementById(tablistName + "-" + tabTitle).style.display = "block";
 	document.getElementById(tabTitle).className += " active"; // This works if the tab itself wasn't clicked but a link
 }
+
+//Scroll button functions
+var scrollAmt = 100;
+function tablistScrollLeft(evt) {
+	var tabs = evt.target.parentNode.querySelector(".visible-links");
+	tabs.scrollLeft -= scrollAmt;
+}
+function tablistScrollRight(evt) {
+	var tabs = evt.target.parentNode.querySelector(".visible-links");
+	tabs.scrollLeft += scrollAmt;
+}
+
+//Show/hide scroll buttons to tablist
+function tablistScrollToggle(tabs) {
+	var btnScrollLeft = tabs.parentNode.querySelector(".tablist-scroll-left");
+	var btnScrollRight = tabs.parentNode.querySelector(".tablist-scroll-right");
+	var tabsSL = tabs.scrollLeft
+	var tabsSW = tabs.scrollWidth
+	var tabsCW = tabs.clientWidth
+
+	//Hide buttons if not needed
+	if (tabsSW == tabsCW) {
+		btnScrollLeft.style.display = "none";
+		btnScrollRight.style.display = "none";
+	}
+	if (tabsSL == 0) {
+		btnScrollLeft.style.opacity = 0;
+	} else {
+		btnScrollLeft.style.opacity = 1;
+	}
+	if (tabsSL == tabsSW - tabsCW) {
+		btnScrollLeft.style.opacity = 0;
+	} else {
+		btnScrollLeft.style.opacity = 1;
+	}
+}
+function tablistScrollEvent(evt) {
+	tablistScrollToggle(evt.target);
+}
